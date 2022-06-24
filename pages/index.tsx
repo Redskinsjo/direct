ce2e9fb379e881bbfcf3cdd82d8070d46ae4f12b
Jonathan Carnos <jonathancarnos@MacBook-Pro-de-Jonathan.local>
2022-06-24 18:43:16 +0200

    comment previous addings of google tag manager

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
   return (
     <div className='relative h-full' id='homepage'>
       <Head>
-        <Script src='../analytics.ts'></Script>
+        {/* <Script src='../analytics.ts'></Script> */}
         <meta
           property='og:title'
           content="Direct Hotel Business: Réserver votre chambre d'hôtel moins cher qu'avant"
@@ -32,53 +32,53 @@ const Home: NextPage = () => {
           content='eYz9plGMyApOd7l6TNDbC9hv1f21l_fQpnA9CgcOviA'
         />
       </Head>
-      <body>
-        <div className='absolute mx-16 my-2'>
-          <span className='absolute text-white font-parisienne text-4xl z-20 top-4 left-2 slidein-30'>
-            Direct
-          </span>
-          <Image
-            src={Logo}
-            width={180}
-            height={90}
-            layout='raw'
-            alt='Logo official'
-            className='slidein-200 relative top-2 z-10 bottom-8'
-          />
-        </div>
-        <iframe
-          src='https://iframe.videodelivery.net/b7ca63b8f05e81406d34be8b8016924b?autoplay=true&muted=true&loop=true&controls=false&preload=auto'
-          className={'w-full h-full bg-transparent absolute border-0'}
-          allowFullScreen={true}
-        ></iframe>
+      {/* <body> */}
+      <div className='absolute mx-16 my-2'>
+        <span className='absolute text-white font-parisienne text-4xl z-20 top-4 left-2 slidein-30'>
+          Direct
+        </span>
+        <Image
+          src={Logo}
+          width={180}
+          height={90}
+          layout='raw'
+          alt='Logo official'
+          className='slidein-200 relative top-2 z-10 bottom-8'
+        />
+      </div>
+      <iframe
+        src='https://iframe.videodelivery.net/b7ca63b8f05e81406d34be8b8016924b?autoplay=true&muted=true&loop=true&controls=false&preload=auto'
+        className={'w-full h-full bg-transparent absolute border-0'}
+        allowFullScreen={true}
+      ></iframe>
 
-        <div className='w-58 h-16 absolute top-32 left-44'>
-          <div className='text-[#fff] text-3xl slidein-30'>{t('hero-p')}</div>
-          <div className='text-lg text-[#eee] slidein-30'>
-            {t('hero-details')}
-            <span className='mx-1'>-</span>
-            <span className='text-[gold]'>Bêta</span>
-          </div>
-          <button
-            onClick={() => {
-              setDiscover(true);
-            }}
-            className='mt-2 ml-8 p-2 rounded-lg button-gold hover:bg-[#FFD700dc] text-xl slidein-200'
-          >
-            {t('hero-action')}
-          </button>
+      <div className='w-58 h-16 absolute top-32 left-44'>
+        <div className='text-[#fff] text-3xl slidein-30'>{t('hero-p')}</div>
+        <div className='text-lg text-[#eee] slidein-30'>
+          {t('hero-details')}
+          <span className='mx-1'>-</span>
+          <span className='text-[gold]'>Bêta</span>
         </div>
-        {discover && <FormBeta />}
-        <Language />
-      </body>
-      <noscript>
+        <button
+          onClick={() => {
+            setDiscover(true);
+          }}
+          className='mt-2 ml-8 p-2 rounded-lg button-gold hover:bg-[#FFD700dc] text-xl slidein-200'
+        >
+          {t('hero-action')}
+        </button>
+      </div>
+      {discover && <FormBeta />}
+      <Language />
+      {/* </body> */}
+      {/* <noscript>
         <iframe
           src='https://www.googletagmanager.com/ns.html?id=GTM-WZ9QF5G'
           height='0'
           width='0'
           style={{ display: 'none', visibility: 'hidden' }}
         ></iframe>
-      </noscript>
+      </noscript> */}
     </div>
   );
 };
